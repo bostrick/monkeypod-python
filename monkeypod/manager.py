f4dea31977c65783cec4d90381b4aaf70ce7a773
Bowe Strickland <bowe@redhat.com>
2024-04-28 19:29:44 -0400

    stripe entity management reworking

--- monkeypod/manager.py
+++ monkeypod/manager.py
@@ -23,9 +23,7 @@ import logging
 import collections
 import csv
 import re
-import os
 from pathlib import Path
-from functools import cached_property
 
 import attr
 import arrow
@@ -42,11 +40,11 @@ class MonkeyPodManager:
 
     client = attr.ib()
 
-    import_path = HERE / "data/imports/monkey_pod_columns.yaml"
-
-    @cached_property
-    def import_column_map(self):
-        return yaml.safe_load(self.import_path.read_text())
+#    import_path = HERE / "data/imports/monkey_pod_columns.yaml"
+#
+#    @cached_property
+#    def import_column_map(self):
+#        return yaml.safe_load(self.import_path.read_text())
 
     def _extract_data(self, item, attr_map):
         # forces all entries to strings
@@ -57,68 +55,68 @@ class MonkeyPodManager:
         ndata = {k: v for k, v in ndata.items() if v}
         return ndata
 
-    def import_entities(
-        self,
-        entity_list,
-        attr_map=None,
-        source_attr=None,
-        import_attr=None,
-    ):
-        """Import a list of entities.
-
-        Skip the entitiy if the email already exists.
-
-        'source_attr' and 'import_attr' get added as 'source' and 'import'
-        extra attributes.
-
-        attr_map can be used to reshape attrs from another input shape.
-        """
-
-        nadded = 0
-        for entity in entity_list:
-
-            if attr_map:
-                entity = self._extract_data(entity, attr_map)
-
-            email = entity.get('email')
-            name = entity.get('name')
-            # TODO... merge additional attributes into entity
-            if email:
-                match = self.client.entity_match(email=email)
-                if match:
-                    LOG.info(f"skipping {email}: exists")
-                    continue
-            elif name:
-                if name:
-                    match = self.client.entity_match(name=name)
-                    if match:
-                        LOG.info(f"skipping {name}: exists")
-                        continue
-            else:
-                LOG.debug("skipping no name or email")
-                continue
-
-            entity.setdefault("type", "Individual")
-
-            extra = entity.setdefault("extra_attributes", {})
-            if source_attr:
-                extra.setdefault("source", source_attr)
-            if import_attr:
-                extra.setdefault("import", import_attr)
-
-            roles = set(entity.setdefault("roles", []))
-            roles.add("Donor")
-            roles.add("Customer")
-            entity['roles'] = list(roles)
-
-            LOG.info(f"creating entity {email} {name}")
-            LOG.info(os.linesep+yaml.safe_dump(entity))
-            yesno = input("import entity? (y/N)").strip().lower()
-            if yesno and yesno[0] == 'y':
-                _ = self.client.entity_create(entity)
-                nadded += 1
-
-        return nadded
+#    def import_entities(
+#        self,
+#        entity_list,
+#        attr_map=None,
+#        source_attr=None,
+#        import_attr=None,
+#    ):
+#        """Import a list of entities.
+#
+#        Skip the entitiy if the email already exists.
+#
+#        'source_attr' and 'import_attr' get added as 'source' and 'import'
+#        extra attributes.
+#
+#        attr_map can be used to reshape attrs from another input shape.
+#        """
+#
+#        nadded = 0
+#        for entity in entity_list:
+#
+#            if attr_map:
+#                entity = self._extract_data(entity, attr_map)
+#
+#            email = entity.get('email')
+#            name = entity.get('name')
+#            # TODO... merge additional attributes into entity
+#            if email:
+#                match = self.client.entity_match(email=email)
+#                if match:
+#                    LOG.info(f"skipping {email}: exists")
+#                    continue
+#            elif name:
+#                if name:
+#                    match = self.client.entity_match(name=name)
+#                    if match:
+#                        LOG.info(f"skipping {name}: exists")
+#                        continue
+#            else:
+#                LOG.debug("skipping no name or email")
+#                continue
+#
+#            entity.setdefault("type", "Individual")
+#
+#            extra = entity.setdefault("extra_attributes", {})
+#            if source_attr:
+#                extra.setdefault("source", source_attr)
+#            if import_attr:
+#                extra.setdefault("import", import_attr)
+#
+#            roles = set(entity.setdefault("roles", []))
+#            roles.add("Donor")
+#            roles.add("Customer")
+#            entity['roles'] = list(roles)
+#
+#            LOG.info(f"creating entity {email} {name}")
+#            LOG.info(os.linesep+yaml.safe_dump(entity))
+#            yesno = input("import entity? (y/N)").strip().lower()
+#            if yesno and yesno[0] == 'y':
+#                _ = self.client.entity_create(entity)
+#                nadded += 1
+#
+#        return nadded
 
     stripe_mp_entity_map = yaml.safe_load("""
         email: billing_details.email
@@ -131,20 +129,61 @@ class MonkeyPodManager:
         # address2: billing_details.address.line2
     """)
 
-    def _confirm_stripe_entity(
-        self, item, source_attr=None, import_attr=None
-    ):
+#    def _unknown_entities_itr(self, stripe_items):
+#        for item in stripe_items:
+#            mp_entity = self._extract_stripe_entity(item)
+#            if not self._mp_entity_exists(mp_entity):
+#                yield self._normalize_mp_entity(mp_entity)
+
+    def _extract_mp_entity_from_stripe_tx(self, stripe_tx):
         entity = self._extract_path_map(
-            item, self.stripe_mp_entity_map,
+            stripe_tx, self.stripe_mp_entity_map,
         )
         entity = {k: v.strip() for k, v in entity.items() if v}
         entity = {k: v for k, v in entity.items() if v}
-        self.import_entities(
-            [entity],
-            source_attr=source_attr,
-            import_attr=import_attr,
-        )
         return entity
+
+    def _mp_entity_exists(self, entity):
+
+        email = entity.get('email')
+        if email:
+            match = self.client.entity_match(email=email)
+            if match:
+                LOG.info(f"mp {email} exists")
+                return True
+
+        name = entity.get('name')
+        if name:
+            match = self.client.entity_match(name=name)
+            if match:
+                LOG.info(f"mp {name} exists")
+                return True
+
+        return False
+
+    def _normalize_mp_entity(self, entity):
+
+        entity.setdefault("type", "Individual")
+
+        # extra = entity.setdefault("extra_attributes", {})
+
+        roles = set(entity.setdefault("roles", []))
+        roles.add("Donor")
+        roles.add("Customer")
+        entity['roles'] = list(roles)
+
+        return entity
+
+#    def _confirm_stripe_entity(
+#        self, item, source_attr=None, import_attr=None
+#    ):
+#        entity = self._extract_stipe_entity(item)
+#        self.import_entities(
+#            [entity],
+#            source_attr=source_attr,
+#            import_attr=import_attr,
+#        )
+#        return entity
 
     def _extract_path_map(self, src, path_map):
         dst = {}
@@ -157,19 +196,35 @@ class MonkeyPodManager:
     # stripe imports
     #####################################################################
 
-    row_record_paths = yaml.safe_load("""
-        expense account
-        class
-        tags
-        memo
-        Created (UTC)
-        Description
-        Amount
-        Fee
-        id
-    """)
-
     stripe_import_map = yaml.safe_load("""
+      relationship:
+        paths:
+          First Name: name      # needs postprocessing
+          Last Name: name       # needs postprocessing
+          Address1: address
+          City: city
+          State: state
+          Zip: postal_code
+          Country: country
+          Email: email
+        constants:
+          Type: Individual
+          Source: stripe
+          Roles: Donor,Customer
+        fields:
+        - Type
+        - Email
+        - First Name
+        - Last Name
+        - Address1
+        - City
+        - State
+        - Zip
+        - Country
+        - Phone
+        - Roles
+        - Source
+        - Import
       transfer:
         paths:
            Date: created
@@ -256,26 +311,35 @@ class MonkeyPodManager:
     """)
 
     def gen_stripe_imports_from_recs(
-        self, records, tag=None, confirm_entities=False,
+        self, stripe_transactions, tag=None, confirm_entities=False,
     ):
 
         tag = tag or "stripe_" + arrow.utcnow().format("YYYYMMDD")
         collector = collections.defaultdict(list)
         fee_collector = collections.defaultdict(lambda: 0.0)
+        n_entities = n_new_entities = 0
 
-        for rec in records:
-            if confirm_entities:
-                self._confirm_stripe_entity(
-                    rec,
-                    source_attr="stripe",
-                    import_attr=tag,
-                )
-            what, row = self._stripe_generate_import_row(rec)
+        for stripe_tx in stripe_transactions:
+
+            mp_entity = self._extract_mp_entity_from_stripe_tx(stripe_tx)
+            if mp_entity:
+                n_entities += 1
+                if not (
+                    confirm_entities and self._mp_entity_exists(mp_entity)
+                ):
+                    _, r_row = self._generate_relationship(mp_entity)
+                    r_row["Import"] = tag
+                    collector['relationship'].append(r_row)
+                    n_new_entities += 1
+
+            what, row = self._stripe_generate_import_row(stripe_tx)
             collector[what].append(row)
-            self._collect_fee(fee_collector, rec)
+            self._collect_fee(fee_collector, stripe_tx)
 
         data = dict(collector)
         data["fee"] = self._reduce_fees(fee_collector)
+
+        LOG.info(f"import {n_new_entities} out of {n_entities} entities")
 
         # reshape data map to include data and fields
         _sim = self.stripe_import_map
@@ -337,6 +401,13 @@ class MonkeyPodManager:
         self._normalize_record(dst)
         return dst
 
+    def _generate_relationship(self, record):
+        name_tokens = record.get("name", "").split()
+        dst = self._generate_base("relationship", record)
+        dst["First Name"] = " ".join(name_tokens[:-1])
+        dst["Last Name"] = name_tokens[-1]
+        return "relationship", dst
+
     def _generate_transfer(self, record):
         dst = self._generate_base("transfer", record)
         dst["Amount"] = -dst["Amount"]
@@ -369,231 +440,6 @@ class MonkeyPodManager:
         rows = [{'Date': k, 'Amount': v} for k, v in collector.items()]
         rows = [self._generate_base("fee", r) for r in rows]
         return rows
-
-    def _write_csv(self, what, tag, fields, records):
-
-        fname = f"stripe_{what}_{tag}.csv"
-        LOG.info(f"writing {len(records)} donations to {fname}")
-        with open(fname, "w") as f:
-            writer = csv.DictWriter(f, fieldnames=fields)
-            writer.writeheader()
-            [writer.writerow(r) for r in records]
-
-        return fname
-
-    #####################################################3
-    # legacy stripe import from csv
-    #####################################################3
-
-    def gen_stripe_imports(self, rows, tag=None):
-
-        tag = tag or arrow.utcnow().format("YYYYMMDD")
-
-        # filter out empty rows
-        try:
-            rows = [r for r in rows if "Description" in r]
-        except Exception:
-            breakpoint()
-
-        # assign row numbers and normalize
-        for idx, r in enumerate(rows):
-            r["_row_number"] = idx
-            self._normalize_row(r)
-
-        # qualify the transactions
-        parts = self._partition_rows(rows)
-
-        # process transactions
-        self._report_unknown(parts["unknown"])
-        self._gen_stripe_donations_csv(parts["donations"], tag)
-        self._gen_stripe_transfers_csv(parts["transfers"], tag)
-        self._gen_stripe_sales_csv(parts["charges"], tag)
-        self._gen_stripe_fees_csv(rows, tag)
-
-    def _normalize_row(self, row):
-        row["Amount"] = row["Amount"].replace(",", "")
-        row["Fee"] = row["Fee"].replace(",", "")
-        if not row.get("email", "").strip():
-            email = row["Description"].split()[-1]
-            if "@" in email:
-                row["email"] = email
-
-    def _partition_rows(self, rows):
-
-        results = collections.defaultdict(list)
-        for row in rows:
-            desc = row["Description"]
-            if "PAYOUT" in desc:
-                results["transfers"].append(row)
-            elif desc.startswith("Donation by"):
-                results["donations"].append(row)
-            elif desc.startswith("Charge for"):
-                results["charges"].append(row)
-            elif desc.startswith("Invoice"):
-                results["invoices"].append(row)
-            else:
-                results["unknown"].append(row)
-
-        counts = {k: len(v) for k, v in results.items()}
-        LOG.info(f"read transactions: {counts}")
-        return results
-
-    def _report_unknown(self, rows):
-        for r in rows:
-            LOG.warning(f"unknown transaction: {r}")
-
-    def _gen_stripe_donations_csv(self, rows, tag):
-
-        def _gen_row(data):
-
-            return {
-                'Date': arrow.get(data["Created (UTC)"]).format("YYYY-MM-DD"),
-                'Donor': data["email"],
-                'Amount': data["Amount"],
-                'Income Account': "Contributions",
-                'Asset Account': "Stripe Account",
-                'External ID': data["id"],
-                'Ref Number': data["id"],
-                'Payment Method': "Other",
-                'Memo': data.get("memo", ""),
-                'Class': data.get("class", ""),
-                'Tags': data.get("tags", ""),
-            }
-
-        fields = [
-            'Date',
-            'Donor',
-            'Amount',
-            'Income Account',
-            'Asset Account',
-            'External ID',
-            'Ref Number',
-            'Payment Method',
-            'Memo',
-            'Class',
-            'Tags',
-        ]
-
-        fname = f"stripe_donations_{tag}.csv"
-        LOG.info(f"writing {len(rows)} donations to {fname}")
-        with open(fname, "w") as f:
-            writer = csv.DictWriter(f, fieldnames=fields)
-            writer.writeheader()
-            [writer.writerow(_gen_row(r)) for r in rows]
-
-        return fname
-
-    def _gen_stripe_transfers_csv(self, rows, tag):
-
-        def _gen_row(data):
-            return {
-                'Date': arrow.get(data["Created (UTC)"]).format("YYYY-MM-DD"),
-                'Amount': -float(data["Amount"]),
-                'To Account': "Stripe Account",
-                'From Account': "Wells Fargo - Main 1582",
-                'External ID': data["id"],
-                'Ref Number': data["id"],
-                'Memo': data["Description"],
-            }
-
-        fields = [
-            "Date",
-            "Amount",
-            "From Account",
-            "To Account",
-            "External ID",
-            "Ref Number",
-            "Memo",
-        ]
-
-        fname = f"stripe_transfers_{tag}.csv"
-        LOG.info(f"writing {len(rows)} transfers to {fname}")
-        with open(fname, "w") as f:
-            writer = csv.DictWriter(f, fieldnames=fields)
-            writer.writeheader()
-            [writer.writerow(_gen_row(r)) for r in rows]
-
-        return fname
-
-    def _gen_stripe_sales_csv(self, rows, tag):
-
-        def _gen_row(data):
-
-            return {
-                'Date': arrow.get(data["Created (UTC)"]).format("YYYY-MM-DD"),
-                'Customer': data["email"],
-                'Total': data["Amount"],
-                'Item': data["item"],
-                'Quantity': data["quantity"],
-                'Asset Account': "Stripe Account",
-                'External ID': data["id"],
-                'Payment Method': "Other",
-                'Memo': data.get("memo", ""),
-                'Class': data.get("class", ""),
-                'Tags': data.get("tags", ""),
-            }
-
-        fields = [
-            'Date',
-            'Customer',
-            'Total',
-            'Item',
-            'Quantity',
-            'Asset Account',
-            'External ID',
-            'Payment Method',
-            'Memo',
-            'Class',
-            'Tags',
-        ]
-
-        fname = f"stripe_sales_{tag}.csv"
-        LOG.info(f"writing {len(rows)} sales to {fname}")
-        with open(fname, "w") as f:
-            writer = csv.DictWriter(f, fieldnames=fields)
-            writer.writeheader()
-            [writer.writerow(_gen_row(r)) for r in rows]
-
-        return fname
-
-    def _gen_stripe_fees_csv(self, rows, tag):
-
-        transactions = collections.defaultdict(list)
-        for r in rows:
-            when = arrow.get(r["Created (UTC)"])
-            bucket = when.format("YYYY-MM")
-            transactions[bucket].append(float(r["Fee"]))
-
-        data = []
-        for k in sorted(transactions):
-            data.append({
-                'Payee': "stripe",
-                'Date': f"{k}-28",
-                'Amount': sum(transactions[k]),
-                'Expense Account': "Bank Fees",
-                'Checking Account': "Stripe Account",
-                'Ref Number': f"calculated stripe_fees_{k}",
-                'External ID': f"calculated stripe_fees_{k}",
-            })
-
-        fields = [
-            'Payee',
-            'Date',
-            'Amount',
-            'Expense Account',
-            'Checking Account',
-            'Ref Number',
-            'External ID',
-        ]
-
-        fname = f"stripe_fees_{tag}.csv"
-        LOG.info(f"writing {len(data)} fees to {fname}")
-        with open(fname, "w") as f:
-            writer = csv.DictWriter(f, fieldnames=fields)
-            writer.writeheader()
-            [writer.writerow(d) for d in data]
-
-        return fname
 
     #############################################
     # Quickbooks Importing
